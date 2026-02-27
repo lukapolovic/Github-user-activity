479b8b892ae6fb67f1f50ddae14ecd749f087b4c
polovni <luka.polovic11@gmail.com>
2026-02-27 19:56:08 +0100

    Checking user input for illegal characters

--- Main.java
+++ Main.java
@@ -1,9 +1,28 @@
+import java.util.regex.*;
+
 class Main {
 	public static void main(String[] args) {
 		if (args.length > 0) {
-			System.out.println("User provided");
+			String username = args[0];
+			StringCleaner strCleaner = new StringCleaner();
+			if (strCleaner.containsIllegalChar(username)) {
+				System.out.println("Username contains illegal characters!");
+				throw new IllegalArgumentException(username);
+			}
+			String url = "https://api.github.com/users/" + args[0] + "/events/public";
+			System.out.println(url);
 		} else {
 			System.out.println("No user provided");
 		}
+	}
+}
+
+class StringCleaner {
+	public String illegalChars = "[~#@*+%{}<>\\[\\]|\"\\_^]";
+
+	public boolean containsIllegalChar(String toExamine) {
+		Pattern pattern = Pattern.compile(illegalChars);
+		Matcher matcher = pattern.matcher(toExamine);
+		return matcher.find();
 	}
 }
