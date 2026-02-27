ba21160bbd512b1976ce4b85e93c64e4523d7281
polovni <luka.polovic11@gmail.com>
2026-02-27 19:23:15 +0100

    Accepting user input

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+	public static void main(String[] args) {
+		if (args.length > 0) {
+			System.out.println("User provided");
+		} else {
+			System.out.println("No user provided");
+		}
+	}
+}
